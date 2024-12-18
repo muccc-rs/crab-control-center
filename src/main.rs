113e620d90241359b5aae1af627d8c9b7d0bde7c
Rahix <rahix@rahix.de>
2024-12-18 09:48:07 +0100

    Add fake crab mode where fieldbus is not initialized

--- src/main.rs
+++ src/main.rs
@@ -32,51 +32,63 @@ fn main() {
         .format_timestamp_micros()
         .init();
 
-    let mut fieldbus = fieldbus::Fieldbus::new();
+    let mut fieldbus = if std::env::var("FAKE_CRAB")
+        .map(|v| v.parse::<bool>().unwrap())
+        .unwrap_or_default()
+    {
+        // Fake crab gets no fieldbus
+        None
+    } else {
+        Some(fieldbus::Fieldbus::new())
+    };
     let mut logic = logic::Logic::new();
 
-    fieldbus.enter_state(fieldbus::OperatingState::Operate);
+    if let Some(fieldbus) = &mut fieldbus {
+        fieldbus.enter_state(fieldbus::OperatingState::Operate);
+    }
 
     loop {
-        fieldbus.with_process_images(|pii, piq| {
-            use process_image::{tag, tag_mut};
+        if let Some(fieldbus) = &mut fieldbus {
+            fieldbus.with_process_images(|pii, piq| {
+                use process_image::{tag, tag_mut};
 
-            // -KEC1-K1 DO1
-            *tag_mut!(piq, X, 0, 0) = logic.outputs().channels.bottom_front;
-            // -KEC1-K1 DO2
-            *tag_mut!(piq, X, 0, 1) = logic.outputs().channels.bottom_back;
-            // -KEC1-K1 DO3
-            *tag_mut!(piq, X, 0, 2) = logic.outputs().channels.pupil_down;
-            // -KEC1-K1 DO4
-            *tag_mut!(piq, X, 0, 2) = logic.outputs().channels.pupil_top;
+                // -KEC1-K1 DO1
+                *tag_mut!(piq, X, 0, 0) = logic.outputs().channels.bottom_front;
+                // -KEC1-K1 DO2
+                *tag_mut!(piq, X, 0, 1) = logic.outputs().channels.bottom_back;
+                // -KEC1-K1 DO3
+                *tag_mut!(piq, X, 0, 2) = logic.outputs().channels.pupil_down;
+                // -KEC1-K1 DO4
+                *tag_mut!(piq, X, 0, 2) = logic.outputs().channels.pupil_top;
 
-            // -KEC1-K2 DO1
-            *tag_mut!(piq, X, 1, 0) = logic.outputs().channels.eyes;
-            // -KEC1-K2 DO2
-            *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_mid;
-            // -KEC1-K2 DO3
-            *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_bottom;
-            // -KEC1-K2 DO4
-            *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_top;
+                // -KEC1-K2 DO1
+                *tag_mut!(piq, X, 1, 0) = logic.outputs().channels.eyes;
+                // -KEC1-K2 DO2
+                *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_mid;
+                // -KEC1-K2 DO3
+                *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_bottom;
+                // -KEC1-K2 DO4
+                *tag_mut!(piq, X, 1, 1) = logic.outputs().channels.mouth_top;
 
-            // -KEC1-K3 DO1
-            *tag_mut!(piq, X, 2, 0) = logic.outputs().channels.spikes_left;
-            // -KEC1-K3 DO2
-            *tag_mut!(piq, X, 2, 1) = logic.outputs().channels.spikes_mid;
-            // -KEC1-K3 DO3
-            *tag_mut!(piq, X, 2, 3) = logic.outputs().channels.spikes_right;
+                // -KEC1-K3 DO1
+                *tag_mut!(piq, X, 2, 0) = logic.outputs().channels.spikes_left;
+                // -KEC1-K3 DO2
+                *tag_mut!(piq, X, 2, 1) = logic.outputs().channels.spikes_mid;
+                // -KEC1-K3 DO3
+                *tag_mut!(piq, X, 2, 3) = logic.outputs().channels.spikes_right;
 
-            // -KEC1-K5 DO1 (inverted!)
-            *tag_mut!(piq, X, 4, 0) = !logic.outputs().indicator_fault;
-            // -KEC1-K5 DO2
-            *tag_mut!(piq, X, 4, 1) = logic.outputs().indicator_refill_air;
+                // -KEC1-K5 DO1 (inverted!)
+                *tag_mut!(piq, X, 4, 0) = !logic.outputs().indicator_fault;
+                // -KEC1-K5 DO2
+                *tag_mut!(piq, X, 4, 1) = logic.outputs().indicator_refill_air;
 
-            // -KEC1-K6 DI1
-            logic.inputs_mut().dc_ok = tag!(pii, X, 1, 0);
+                // -KEC1-K6 DI1
+                logic.inputs_mut().dc_ok = tag!(pii, X, 1, 0);
 
-            // -KEC1-K7 AI1
-            logic.inputs_mut().pressure_fullscale = tag!(pii, W, 2);
-        });
+                // -KEC1-K7 AI1
+                logic.inputs_mut().pressure_fullscale = tag!(pii, W, 2);
+            });
+        }
 
         logic.run(std::time::Instant::now());
 
