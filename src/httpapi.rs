3709e25cd3500b36d7ec06b8f00083e0e10e1deb
Maximilian Berger <max@geile.software>
2024-12-28 02:31:42 +0100

    Send text to crab and make it feel things

--- src/httpapi.rs
+++ src/httpapi.rs
@@ -11,7 +11,10 @@ use utoipa::OpenApi;
 use utoipa_redoc::Servable as RedocServable;
 use utoipa_scalar::Servable as ScalarServable;
 
-use crate::{emotionmanager, logic};
+use crate::{
+    emotionmanager,
+    logic::{self, Emotion},
+};
 
 const BIND_ADDR: &str = "0.0.0.0:80";
 
@@ -45,24 +48,73 @@ async fn post_emotion(
     State(state): State<AppState>,
     Json(payload): Json<ApiEmotionMessage>,
 ) -> impl IntoResponse {
+    match send_emotion_to_crab(state.emotion_ch_tx.clone(), payload.emotion).await {
+        Ok(status) => status,
+        Err(e) => e,
+    }
+}
+
+async fn send_emotion_to_crab(
+    emotion_ch_tx: tokio::sync::mpsc::Sender<emotionmanager::EmotionCommand>,
+    emotion: Emotion,
+) -> Result<StatusCode, StatusCode> {
     let (tx, rx) = tokio::sync::oneshot::channel();
-    let set_emotion_result = state
-        .emotion_ch_tx
-        .send(emotionmanager::EmotionCommand::Set {
-            emotion: payload.emotion,
-            resp: tx,
-        })
+    let set_emotion_result = emotion_ch_tx
+        .send(emotionmanager::EmotionCommand::Set { emotion, resp: tx })
         .await;
 
-    if let Err(e) = set_emotion_result {
-        return (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e));
+    if let Err(_) = set_emotion_result {
+        println!("Error sending emotion to crab");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
     }
 
-    if let Err(e) = rx.await {
-        return (StatusCode::INTERNAL_SERVER_ERROR, format!("Error: {}", e));
+    // Wait for the emotion to be set
+    if let Err(_) = rx.await {
+        println!("Error waiting for emotion to be set");
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
     }
 
-    (StatusCode::OK, "ok".to_string())
+    Ok(StatusCode::OK)
+}
+
+async fn text_to_emotion(text: &str) -> Emotion {
+    let text = text.to_lowercase();
+    if text.contains("rust") || text.contains("rs") {
+        Emotion::Happy
+    } else if text.contains("golang") || text.contains("go") {
+        Emotion::Angered
+    } else if text.contains("cobol") {
+        Emotion::Surprised
+    } else {
+        Emotion::Neutral
+    }
+}
+
+#[derive(utoipa::ToSchema, serde::Deserialize)]
+struct ApiTalkMessage {
+    message: String,
+}
+
+#[utoipa::path(post,
+    path = "/crab/talk",
+    summary = "Talk to the crab!",
+    request_body = ApiTalkMessage,
+    responses(
+        (status = 200, description = "Success!", body = ())
+))]
+async fn post_crab_talk(
+    State(state): State<AppState>,
+    Json(payload): Json<ApiTalkMessage>,
+) -> impl IntoResponse {
+    // TODO: Figure out what makes crabs feel things
+
+    let text = payload.message;
+    let emotion = text_to_emotion(&text).await;
+
+    match send_emotion_to_crab(state.emotion_ch_tx.clone(), emotion).await {
+        Ok(status) => status,
+        Err(e) => e,
+    }
 }
 
 async fn root(State(_): State<AppState>) -> impl IntoResponse {
@@ -95,6 +147,7 @@ fn app() -> axum::Router<AppState> {
     let (router, api): (axum::Router<AppState>, utoipa::openapi::OpenApi) =
         utoipa_axum::router::OpenApiRouter::with_openapi(ApiDoc::openapi())
             .routes(routes)
+            .routes(utoipa_axum::routes!(post_crab_talk))
             .route("/", get(root))
             .split_for_parts();
 
