muccc-rs/crab-control-center

commit 774591b92ebab95c172dbd7684ed59cae9109328

logic: Make ESTOP signal NC instead of NO
The hardware was finally replaced, the second ESTOP contact is now also
NC so the signal will be low when pressed.  Adjust the code and tag
naming accordingly.

src/main.rs
@@ -115,7 +115,7 @@ fn main() {
                         // -KEC1-K6 DI1
                         logic.inputs_mut().dc_ok = tag!(pii, X, 1, 0);
                         // -KEC1-K6 DI2
-                        logic.inputs_mut().estop_active = tag!(pii, X, 1, 1);
+                        logic.inputs_mut().estop_ok = tag!(pii, X, 1, 1);
 
                         // -KEC1-K7 AI1
                         logic.inputs_mut().pressure_fullscale = tag!(pii, W, 2).into();
@@ -128,7 +128,7 @@ fn main() {
                 {
                     // Some sane defaults when no actual hardware is present
                     logic.inputs_mut().dc_ok = true;
-                    logic.inputs_mut().estop_active = false;
+                    logic.inputs_mut().estop_ok = false;
                     logic.inputs_mut().pressure_fullscale = 64;
                 }
 

src/logic.rs
@@ -67,7 +67,7 @@ pub struct LogicInputs {
     pub emotion: Option<Emotion>,
     pub dc_ok: bool,
     pub pressure_fullscale: i32,
-    pub estop_active: bool,
+    pub estop_ok: bool,
     pub trigger_fan: bool,
     pub trigger_sleep: bool,
     pub reset_fault: bool,
@@ -299,7 +299,7 @@ impl Logic {
             || pressure_fault
             || fan_overtime
             || self.pressure_high_high
-            || self.inp.estop_active
+            || !self.inp.estop_ok
             || !self.logic_initialized
             || !self.inp.dc_ok;
 

commit cb657d27a6aecdd7e5f4a5433fdf6119c2964659

In simulation, correctly pretend the E-Stop is fine
The polarity of the e-stop signal was changed in commit 774591b92eba
("logic: Make ESTOP signal NC instead of NO"). Correctly set the
replacement value in simulation as well.

Fixes: 774591b92eba ("logic: Make ESTOP signal NC instead of NO")

src/main.rs
@@ -147,7 +147,7 @@ fn main() {
                 {
                     // Some sane defaults when no actual hardware is present
                     logic.inputs_mut().dc_ok = true;
-                    logic.inputs_mut().estop_ok = false;
+                    logic.inputs_mut().estop_ok = true;
                     logic.inputs_mut().pressure_fullscale = 64;
                 }
 